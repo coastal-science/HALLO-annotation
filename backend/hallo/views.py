faa598d5e1e1b93f58f6c263024b7b377288fac7
Fabio Frazao <fsfrazao@dal.ca>
2022-11-15 13:28:25 -0400

    hallo-views.py-process_segment_image-> Call the adjust_range function on the spectrogram array when creating the figure

--- backend/hallo/views.py
+++ backend/hallo/views.py
@@ -41,7 +41,7 @@ def process_segment_image(audio_file, start, end, spec_config, spec_output, spec
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
-    ax.imshow(spec.data.T, origin='lower', vmin=vmin, vmax=vmax, cmap=cmap)
+    ax.imshow(adjust_array(spec.data.T), origin='lower', vmin=vmin, vmax=vmax, cmap=cmap)
 
     plt.savefig(spec_output, dpi=spec_dpi, pad_inches=0, bbox_inches='tight')
 
